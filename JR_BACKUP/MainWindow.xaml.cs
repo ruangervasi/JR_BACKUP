55d6b33318efc69a0116558fa787d49bda7f1cc4
Ruan Gervasi <ruan@ruangervasi.com.br>
2017-03-30 18:04:39 -0300

    RefatoradoProjetoParaVisual2017

--- JR_BACKUP/MainWindow.xaml.cs
+++ JR_BACKUP/MainWindow.xaml.cs
@@ -8,6 +8,10 @@ using System.ComponentModel;
 using System.Windows.Input;
 using Microsoft.Win32;
 using System.Windows.Forms;
+using System.Timers;
+using System.Security.AccessControl;
+using System.Diagnostics;
+using NMDD_EnviaEmail;
 
 namespace JR_BACKUP
 {
@@ -20,6 +24,8 @@ namespace JR_BACKUP
         {
             InitializeComponent();
         }
+
+        public string backuplocal = string.Empty;
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
@@ -46,6 +52,9 @@ namespace JR_BACKUP
                 if (ConfigBackup[15] == "1") { CheckSabado.IsChecked = true; }
                 if (ConfigBackup[16] == "1") { CheckDomingo.IsChecked = true; }
                 DiaMes.Text = ConfigBackup[17];
+                if (ConfigBackup[18] == "1") { Ativo.IsChecked = true; }
+
+                backuplocal = ConfigBackup[0];
             }
         }
 
@@ -67,81 +76,93 @@ namespace JR_BACKUP
 
         public void btnTestar_Click(object sender, RoutedEventArgs e)
         {
-            
             UseThread1();
-            
         }
 
-        public static void Compress()
+        public static void Compress(string Origem, string Destino)
         {
-            string diretorio = @"c:\tempBackup";//Caminho do diretório
-            string arquivo = @"c:\BACKUPJR_" + DateTime.Now.Day + DateTime.Now.Month + DateTime.Now.Year + "_" + DateTime.Now.Hour + DateTime.Now.Minute + ".zip";//Caminho do arquivo zip a ser criado
-            ZipFile.CreateFromDirectory(diretorio, arquivo); 
+            if (!Directory.Exists(Destino))
+            {
+                Directory.CreateDirectory(Destino);
+            }
+
+            string diretorio = Origem;//Caminho do diretório
+            string arquivo = Destino + @"\BACKUPJR_" + DateTime.Now.Day + DateTime.Now.Month + DateTime.Now.Year + "_" + DateTime.Now.Hour + DateTime.Now.Minute + ".zip";//Caminho do arquivo zip a ser criado
+            ZipFile.CreateFromDirectory(diretorio, arquivo);
+        }
+
+        public static string ExecutarCMD(string comando)
+        {
+            using (Process processo = new Process())
+            {
+                processo.StartInfo.FileName = Environment.GetEnvironmentVariable("comspec");
+
+                // Formata a string para passar como argumento para o cmd.exe
+                processo.StartInfo.Arguments = string.Format("/c {0}", comando);
+
+                processo.StartInfo.RedirectStandardOutput = true;
+                processo.StartInfo.UseShellExecute = false;
+                processo.StartInfo.CreateNoWindow = true;
+
+                processo.Start();
+                processo.WaitForExit();
+
+                string saida = processo.StandardOutput.ReadToEnd();
+                return saida;
+            }
         }
 
         public void FazerBackup()
         {
-            if (Directory.Exists(@"c:\tempBackup"))
+            try
             {
-                Directory.Delete(@"c:\tempBackup", true);
+                if (Directory.Exists(@"c:\tempBackup"))
+                {
+                    Directory.Delete(@"c:\tempBackup", true);
+                }
+
+                if (Directory.Exists(@"c:\gsn"))
+                {
+                    CopiaArquivos(@"c:\gsn", @"c:\tempBackup\gsn");
+                }
+
+                if (Directory.Exists(@"c:\ecf"))
+                {
+                    CopiaArquivos(@"c:\ecf", @"c:\tempBackup\ecf");
+                }
+
+                if (Directory.Exists(@"c:\jrsystem"))
+                {
+                    CopiaArquivos(@"c:\jrsystem", @"c:\tempBackup\jrsystem");
+                }
+
+                if (Directory.Exists(@"c:\mysql\data"))
+                {
+                    CopiaArquivos(@"c:\mysql\data", @"c:\tempBackup\banco");
+                }
+
+                Compress(@"c:\tempBackup",backuplocal);
+
+                if (Directory.Exists(@"c:\tempBackup"))
+                {
+                    Directory.Delete(@"c:\tempBackup", true);
+                }
+
+                EnviaEmail("Backup realizado com sucesso!","Backup JR: ");
             }
-
-            string cliente = "JR PDV SISTEMAS E AUTOMAÇÃO LTDA ME";
-
-            if (Directory.Exists(@"c:\gsn"))
+            catch (Exception e)
             {
-                CopiaArquivos(@"c:\gsn", @"c:\tempBackup\gsn");
+                EnviaEmail(e.ToString(), "Erro no Backup: ");
             }
-
-            if (Directory.Exists(@"c:\ecf"))
-            {
-                CopiaArquivos(@"c:\ecf", @"c:\tempBackup\ecf");
-            }
-
-            if (Directory.Exists(@"c:\jrsystem"))
-            {
-                CopiaArquivos(@"c:\jrsystem", @"c:\tempBackup\jrsystem");
-            }
-
-            if (Directory.Exists(@"c:\mysql\data"))
-            {
-                CopiaArquivos(@"c:\mysql\data", @"c:\tempBackup\banco");
-            }
-
-            Compress();
-
-            if (File.Exists(@"c:\gsn\ConfigEmail.txt"))
-            {
-                File.Delete(@"c:\gsn\ConfigEmail.txt");
-            }
-
-            StreamWriter vWriter = new StreamWriter(@"c:\gsn\ConfigEmail.txt", true);
-            vWriter.WriteLine("SMTP=smtp.gmail.com");
-            vWriter.WriteLine("SSL=True");
-            vWriter.WriteLine("REMETENTENOME=" + cliente);
-            vWriter.WriteLine("REMETENTEEMAIL=logbackupjr@gmail.com");
-            vWriter.WriteLine("REMETENTESENHA=joel0307");
-            vWriter.WriteLine("ASSUNTO=REF.: Backup realizado!");
-            vWriter.WriteLine("DESTINATARIO=logbackupjr@gmail.com");
-            vWriter.WriteLine("CORPOEMAIL=Backup do cliente " + cliente + " Realizado com sucesso!" + DateTime.Now.ToString());
-            vWriter.WriteLine("ASSUNTO=REF.: Backup realizado!");
-            vWriter.WriteLine("ANEXO=");
-            vWriter.Flush();
-            vWriter.Close();
-
-            System.Diagnostics.Process.Start(@"c:\gsn\email.exe");
-
-            if (Directory.Exists(@"c:\tempBackup"))
-            {
-                Directory.Delete(@"c:\tempBackup", true);
-            }
-        }
+        }    
 
         public void CopiaArquivos(string Origem, string Destino)
         {
             if (!Directory.Exists(Destino))
             {
                 Directory.CreateDirectory(Destino);
+                ExecutarCMD("icacls " + Destino + " /grant " + Environment.UserName + ":F");
+                ExecutarCMD("attrib -r +s " + Destino);
             }
 
             if (!string.IsNullOrEmpty(Origem))
@@ -240,51 +261,65 @@ namespace JR_BACKUP
 
         private void SalvarAlteracoes_Click(object sender, RoutedEventArgs e)
         {
-            var linha0 = string.Empty; if (LocalBackup.Text != string.Empty) { linha0 = LocalBackup.Text; } else { linha0 = @"c:\"; }
-            var linha1 = string.Empty; if (RadioDiario.IsChecked == true) { linha1 = "1"; } else if (RadioDiario.IsChecked == false && RadioSemanal.IsChecked == false && RadioMensal.IsChecked == false) { linha1 = "1"; } else { linha1 = "0"; }
-            var linha2 = string.Empty; if (RadioSemanal.IsChecked == true) { linha2 = "1"; } else { linha2 = "0"; }
-            var linha3 = string.Empty; if (RadioMensal.IsChecked == true) { linha3 = "1"; } else { linha3 = "0"; }
-            var linha4 = string.Empty; if (Hora1.Text != "") { linha4 = Hora1.Text; } else { linha4 = "12:00"; }
-            var linha5 = string.Empty; if (Hora2.Text != "") { linha5 = Hora2.Text; } else { linha5 = ":"; }
-            var linha6 = string.Empty; if (Hora3.Text != "") { linha6 = Hora3.Text; } else { linha6 = ":"; }
-            var linha7 = string.Empty; if (Hora4.Text != "") { linha7 = Hora4.Text; } else { linha7 = ":"; }
-            var linha8 = string.Empty; if (Hora5.Text != "") { linha8 = Hora5.Text; } else { linha8 = ":"; }
-            var linha9 = string.Empty; if (Hora6.Text != "") { linha9 = Hora6.Text; } else { linha9 = ":"; }
-            var linha10 = string.Empty; if (CheckSegunda.IsChecked == true && GroupSemanal.IsEnabled == true) { linha10 = "1"; } else { linha10 = "0"; }
-            var linha11 = string.Empty; if (CheckTerca.IsChecked == true && GroupSemanal.IsEnabled == true) { linha11 = "1"; } else { linha11 = "0"; }
-            var linha12 = string.Empty; if (CheckQuarta.IsChecked == true && GroupSemanal.IsEnabled == true) { linha12 = "1"; } else { linha12 = "0"; }
-            var linha13 = string.Empty; if (CheckQuinta.IsChecked == true && GroupSemanal.IsEnabled == true) { linha13 = "1"; } else { linha13 = "0"; }
-            var linha14 = string.Empty; if (CheckSexta.IsChecked == true && GroupSemanal.IsEnabled == true) { linha14 = "1"; } else { linha14 = "0"; }
-            var linha15 = string.Empty; if (CheckSabado.IsChecked == true && GroupSemanal.IsEnabled == true) { linha15 = "1"; } else { linha15 = "0"; }
-            var linha16 = string.Empty; if (CheckDomingo.IsChecked == true && GroupSemanal.IsEnabled == true) { linha16 = "1"; } else { linha16 = "0"; }
-            var linha17 = string.Empty; if (DiaMes.Text != "") { linha17 = DiaMes.Text; } else { linha17 = "0"; }
-
-            if (File.Exists(@"c:\gsn\ConfigBackup.ini"))
+            try
             {
-                File.Delete(@"c:\gsn\ConfigBackup.ini");
-            }
+                var linha0 = string.Empty; if (LocalBackup.Text != string.Empty) { linha0 = LocalBackup.Text; } else { linha0 = @"c:\"; }
+                var linha1 = string.Empty; if (RadioDiario.IsChecked == true) { linha1 = "1"; } else if (RadioDiario.IsChecked == false && RadioSemanal.IsChecked == false && RadioMensal.IsChecked == false) { linha1 = "1"; } else { linha1 = "0"; }
+                var linha2 = string.Empty; if (RadioSemanal.IsChecked == true) { linha2 = "1"; } else { linha2 = "0"; }
+                var linha3 = string.Empty; if (RadioMensal.IsChecked == true) { linha3 = "1"; } else { linha3 = "0"; }
+                var linha4 = string.Empty; if (Hora1.Text != "") { linha4 = Hora1.Text; } else { linha4 = "12:00"; }
+                var linha5 = string.Empty; if (Hora2.Text != "") { linha5 = Hora2.Text; } else { linha5 = ":"; }
+                var linha6 = string.Empty; if (Hora3.Text != "") { linha6 = Hora3.Text; } else { linha6 = ":"; }
+                var linha7 = string.Empty; if (Hora4.Text != "") { linha7 = Hora4.Text; } else { linha7 = ":"; }
+                var linha8 = string.Empty; if (Hora5.Text != "") { linha8 = Hora5.Text; } else { linha8 = ":"; }
+                var linha9 = string.Empty; if (Hora6.Text != "") { linha9 = Hora6.Text; } else { linha9 = ":"; }
+                var linha10 = string.Empty; if (CheckSegunda.IsChecked == true && GroupSemanal.IsEnabled == true) { linha10 = "1"; } else { linha10 = "0"; }
+                var linha11 = string.Empty; if (CheckTerca.IsChecked == true && GroupSemanal.IsEnabled == true) { linha11 = "1"; } else { linha11 = "0"; }
+                var linha12 = string.Empty; if (CheckQuarta.IsChecked == true && GroupSemanal.IsEnabled == true) { linha12 = "1"; } else { linha12 = "0"; }
+                var linha13 = string.Empty; if (CheckQuinta.IsChecked == true && GroupSemanal.IsEnabled == true) { linha13 = "1"; } else { linha13 = "0"; }
+                var linha14 = string.Empty; if (CheckSexta.IsChecked == true && GroupSemanal.IsEnabled == true) { linha14 = "1"; } else { linha14 = "0"; }
+                var linha15 = string.Empty; if (CheckSabado.IsChecked == true && GroupSemanal.IsEnabled == true) { linha15 = "1"; } else { linha15 = "0"; }
+                var linha16 = string.Empty; if (CheckDomingo.IsChecked == true && GroupSemanal.IsEnabled == true) { linha16 = "1"; } else { linha16 = "0"; }
+                var linha17 = string.Empty; if (DiaMes.Text != "") { linha17 = DiaMes.Text; } else { linha17 = "0"; }
+                var linha18 = string.Empty; if (Ativo.IsChecked == true) { linha18 = "1"; } else { linha18 = "0"; }
 
-            StreamWriter vWriter = new StreamWriter(@"c:\gsn\ConfigBackup.ini", true);
-            vWriter.WriteLine(linha0);
-            vWriter.WriteLine(linha1);
-            vWriter.WriteLine(linha2);
-            vWriter.WriteLine(linha3);
-            vWriter.WriteLine(linha4);
-            vWriter.WriteLine(linha5);
-            vWriter.WriteLine(linha6);
-            vWriter.WriteLine(linha7);
-            vWriter.WriteLine(linha8);
-            vWriter.WriteLine(linha9);
-            vWriter.WriteLine(linha10);
-            vWriter.WriteLine(linha11);
-            vWriter.WriteLine(linha12);
-            vWriter.WriteLine(linha13);
-            vWriter.WriteLine(linha14);
-            vWriter.WriteLine(linha15);
-            vWriter.WriteLine(linha16);
-            vWriter.WriteLine(linha17);
-            vWriter.Flush();
-            vWriter.Close();
+                if (File.Exists(@"c:\gsn\ConfigBackup.ini"))
+                {
+                    File.Delete(@"c:\gsn\ConfigBackup.ini");
+                }
+
+                StreamWriter vWriter = new StreamWriter(@"c:\gsn\ConfigBackup.ini", true);
+                vWriter.WriteLine(linha0);
+                vWriter.WriteLine(linha1);
+                vWriter.WriteLine(linha2);
+                vWriter.WriteLine(linha3);
+                vWriter.WriteLine(linha4);
+                vWriter.WriteLine(linha5);
+                vWriter.WriteLine(linha6);
+                vWriter.WriteLine(linha7);
+                vWriter.WriteLine(linha8);
+                vWriter.WriteLine(linha9);
+                vWriter.WriteLine(linha10);
+                vWriter.WriteLine(linha11);
+                vWriter.WriteLine(linha12);
+                vWriter.WriteLine(linha13);
+                vWriter.WriteLine(linha14);
+                vWriter.WriteLine(linha15);
+                vWriter.WriteLine(linha16);
+                vWriter.WriteLine(linha17);
+                vWriter.WriteLine(linha18);
+                vWriter.Flush();
+                vWriter.Close();
+
+                ExecutarCMD("net stop jrbackup");
+                ExecutarCMD("net start jrbackup");
+
+                LabelInfoOther.Content = "Configurações Salvas!";
+            }
+            catch
+            {
+                LabelInfoOther.Content = "Há algo errado, reveja as configurações!!!";
+            }
         }
 
         private void LocalBackup_MouseDown(object sender, System.Windows.Input.MouseEventArgs e)
@@ -295,7 +330,13 @@ namespace JR_BACKUP
 
         private void Cancelar_Click(object sender, RoutedEventArgs e)
         {
+            this.Close();
+        }
 
+        public void EnviaEmail(string Mensagem, string Assunto)
+        {
+            EnviaEmail Email = new EnviaEmail();
+            Email.SendEmail("smtp.gmail.com", true, "BackupJR LOG", "logbackupjr@gmail.com", "joel0307", Assunto, "logbackupjr@gmail.com", Mensagem, "");
         }
     }
 }
